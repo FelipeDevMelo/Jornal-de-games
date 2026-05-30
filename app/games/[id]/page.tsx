50ae1f34d852bc91cb956a8f70a09e6267801167
luizfelipe <luizfelipemelo001@gmail.com>
2026-05-30 14:55:26 -0300

    style: game page

--- app/games/[id]/page.tsx
+++ app/games/[id]/page.tsx
@@ -1,3 +1,39 @@
-export default function GamePage() {
-  return <h1>Página do jogo</h1>;
+import gamesData from "@/app/data/games.json";
+import { Gaegu } from "next/font/google";
+
+interface GamePageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+export default async function GamePage({ params }: GamePageProps) {
+  const { id } = await params;
+  const game = gamesData.find((g) => g.id === id);
+  if (!game) {
+    return <h1>Jogo não encontrado!</h1>;
+  }
+  return (
+    <main className="p-8 font-sans gap-12">
+      <h1 className="text-3xl font-bold uppercase mb-8">{game.title}</h1>
+      <section>
+        <h2 className="text-xl font-bold text-zinc-400 mb-4">
+          1. Techinical Lens
+        </h2>
+        <p>
+          Core loop: <strong>{game.review.technicalLens.coreLoop}</strong>
+        </p>
+      </section>
+
+      <section className="grid grid-cols-2 gap-6 mt-12 ">
+        <div className="text-emerald-950 p-3 bg-emerald-100 rounded-lg border-emerald-200 border-2">
+          <h3>Bug report</h3>
+          <p>{game.review.gameDevVerdict.bugReport}</p>
+        </div>
+        <div className="text-amber-950 p-3 bg-amber-100 rounded-lg border-amber-200 border-2">
+          <h3>Golden ticket</h3>
+          <p>{game.review.gameDevVerdict.goldenTicket}</p>
+        </div>
+      </section>
+    </main>
+  );
 }
